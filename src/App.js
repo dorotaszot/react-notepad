2328719c421156aa19920aa6cdad00f2cef34df3
dorotaszot <62651157+dorotaszot@users.noreply.github.com>
2021-03-09 14:09:48 +0100

    input and texarea handled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import AddSingleNote from './components/AddSingleNote';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [inputs, setInputs] = useState({
+    title: '',
+    note: ''
+  });  
+  
+  const handleTitleInput = (event) => {
+    setInputs({...inputs, title: event.target.value})
+  }
+ 
+  const handleNoteInput = (event) => {
+    setInputs({...inputs, note: event.target.value})
+  }
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
+
   return (
-   <AddSingleNote />
+   <AddSingleNote inputs={inputs} setInputs={setInputs} handleTitleInput={handleTitleInput} handleNoteInput={handleNoteInput} handleOnSubmit={handleOnSubmit}/>
   );
 }
 
--- src/components/AddSingleNote.js
+++ src/components/AddSingleNote.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-const AddSingleNote = () => {
+const AddSingleNote = (props) => {
   return (
-    <div className="d-flex justify-content-center">
-      <form action="#">
+    <div className="d-flex justify-content-start container">
+      <form action="#" onSubmit={props.handleOnSubmit}>
        <div  className="single-note d-flex flex-column m-3">
-        <input type="text" name='title' placeholder='Title' className="p-2 mb-2 form"/>
-        <textarea name="note" id="note" cols="30" rows="10" placeholder="Write a note..." className="p-2 mb-2"></textarea>
+        <input 
+          type="text" name='noteTitle' 
+          placeholder='Title' 
+          className="p-2 mb-2 form" 
+          value={props.inputs.title}
+          onChange={props.handleTitleInput}/>
+        <textarea 
+          name="noteText" id="noteText" 
+          cols="10" rows="7" 
+          placeholder="Write a note..." 
+          className="p-2 mb-2" 
+          value={props.inputs.note}
+          onChange={props.handleNoteInput}>
+        </textarea>
         <button type="submit" className="p-2 mb-2 btn btn-light">Create a note</button>
        </div>
       </form>
     </div>
   )
 }
+
+
 
 export default AddSingleNote
